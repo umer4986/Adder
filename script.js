9b47dd69db8851986fc8ac665e048df650db0b4d
umer4986 <60739319+umer4986@users.noreply.github.com>
2022-04-23 12:50:39 +0500

    none

---
+++ script.js
@@ -0,0 +1,9 @@
+function myFunction(){
+	let number1 = document.querySelector("#userInput1");
+	let number2 = document.querySelector("#userInput2");
+	let result = number1+number2;
+	let message = document.querySelector("#output");
+	message.innerHTML = "1234";
+
+
+}
